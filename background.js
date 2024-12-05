716205568bddb76a1964b8f534fe33f9da8c2ec1
Mercutio <mercutio_john@foxmail.com>
2024-12-05 21:22:05 +0800

    don' need that much comments

--- background.js
+++ background.js
@@ -1,15 +1,11 @@
 chrome.action.onClicked.addListener((tab) => {
   const originalUrl = tab.url;
 
-  // 正则表达式匹配任何 URL
-  // const regex = /^https?:\/\/(.*)/;
   const regex = /^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)([^?\n]*)/i;
 
-  // 检查 URL 是否匹配正则表达式
   const matches = originalUrl.match(regex);
 
   if (matches) {
-    // 提取原始 URL 中的域名和路径
     const domain = matches[1];
     const path = matches[2];
 
